faa27b076fa41010a986d9d016f210fd9a9f84d1
BuildxSaas <156997599+BuildxSaasApp@users.noreply.github.com>
2024-03-06 16:54:53 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Home() {
       <NextSeo
         title="Plutonium"
         description="Plutonium – where darkness meets the web."
-        canonical="https://plutonium.vercel.app/"
+        canonical=""
         openGraph={{
-          url: "https://plutonium.vercel.app/",
+          url: "",
           title: "Plutonium",
           description: "Plutonium – where darkness meets the web.",
           images: [
@@ -26,8 +26,8 @@ export default function Home() {
           site_name: "Plutonium",
         }}
         twitter={{
-          handle: "@saurishhh",
-          site: "https://plutonium.vercel.app/",
+          handle: "",
+          site: "",
           cardType: "summary_large_image",
         }}
       />
@@ -55,7 +55,7 @@ export default function Home() {
                 className="inline-flex items-center px-5 py-3 text-sm font-medium text-gray-300 transition duration-300 bg-black rounded hover:bg-gray-800 dark:hover:bg-gray-200 dark:text-gray-700 dark:bg-white"
                 aria-label="learn more"
                 rel="noreferrer"
-                href="https://github.com/BuildxSaasApp/LP-Plutonium"
+                href=""
               >
                 <span className="flex justify-center">GitHub Link</span>
               </a>
